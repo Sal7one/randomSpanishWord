5e2e9cef91f6bd1347b9ffcb94adb7c5729fa27b
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2022-11-28 10:54:50 +0300

    finished

---
+++ app.js
@@ -0,0 +1,39 @@
+let randomwordbtn = document.querySelector("#morebtn");
+randomwordbtn.addEventListener("click", nextWord)
+cleanedWords = JSON.parse(apiResponse);
+
+function nextWord(){
+  let randomnumber= Math.floor(Math.random() * 1992);
+  let bothWords = cleanedWords[randomnumber];
+  let englishWord = bothWords.substring(0, bothWords.indexOf("–"))
+  let SpanishWord = bothWords.substring(bothWords.indexOf("–")+1);
+  document.querySelector("#word").innerHTML = SpanishWord
+  document.querySelector("#translation").innerHTML = englishWord
+}
+
+nextWord();
+
+
+// Handle touch devices
+let touch_div = document.querySelector('.flip-card');
+let is_touch_device = false;
+let touchstart_event_listener; 	// save the event listeners, if you want to delete them later
+let touchend_event_listener; 		// save the event listeners, if you want to delete them later
+
+if ("ontouchstart" in document.documentElement) {	// check if touch is enabled on device
+  is_touch_device = true;
+}
+
+if (is_touch_device) {
+	touch_div.classList.add('touch-device');	// adds a class to style the div for touch devices
+  
+  touch_div.addEventListener('touchstart', touchstart_event_listener = () => {	// event listener changes the class to hovered when you put your 
+  	touch_div.classList.replace('touch-device', 'touch-device-hovered');				// finger on the div
+  });
+  
+  touch_div.addEventListener('touchend', touchstart_event_listener = () => {		// event listener changes the class back to not hovered when you 
+  	touch_div.classList.replace('touch-device-hovered', 'touch-device');				// release your finger
+  });
+} else {
+	touch_div.classList.add('mouse-device');	// adds a class to style the div for mouse devices
+}
